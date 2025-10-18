f491da0c9f0ca4c17e49d29ccb252774f6e12fa7
Jefferson-AlvesNeres <bloobbloom@gmail.com>
2025-10-18 11:50:38 -0300

    Guia-BSI, Ultima versão do codigo

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 
 // script.js (Versão Completa e Otimizada)
 
-// --- NOVO: CÓDIGO DO VERCELL ANALYTICS ---
-import { inject } from '@vercel/analytics';
-inject();
-// ----------------------------------------
-
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- LÓGICA DO SELETOR DE TEMA (RODA EM TODAS AS PÁGINAS) ---
@@ -64,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
     checkLoginState(); // Verifica o estado de login assim que a página carrega
-
+    
     const logoutBtn = document.getElementById('logout-btn');
     if (logoutBtn) {
         logoutBtn.addEventListener('click', (e) => {
